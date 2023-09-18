1c9876bbc4b0abbdad770ae7b63b61ab9f7f0846
Anderson Rodrigues <anderson.rodrigues@gruposolar.com.br>
2023-09-18 17:15:19 -0300

    Push180920231715

---
+++ resources/views/api.blade.php
@@ -0,0 +1,10 @@
+<div style="width: 80%; background-color: #fff; margin: auto;">
+<p style="
+font-size: 14px; 
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+font-weight: 600;
+font-family: Verdana, Geneva, Tahoma, sans-serif;
+color: #777;
+">Api Laravel Framework 10.16.1 - v1</p>
+</div>
