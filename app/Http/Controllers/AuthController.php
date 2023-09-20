9572ae5cad5aab5af38f8c2d608accc88b16f3ca
anderson brasil <andersonbrasil72@gmail.com>
2023-09-20 20:03:02 -0300

    Push Parte um

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,6 +15,7 @@ class AuthController extends Controller
         if (Auth::attempt($request->only('email', 'password'))) {
             return $this->response('Authorized', 200, [
                 'token' => $request->user()->createToken('appsos')->plainTextToken,
+                'id' =>$request->user()->id,
                 'name' =>$request->user()->name,
                 'email' => $request->user()->email
             ]);
