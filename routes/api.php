0909b3c12b565c7a03363f38e6ad28c110c18f4d
anderson brasil <andersonbrasil72@gmail.com>
2023-09-27 23:43:19 -0300

    Push270920232343

--- routes/api.php
+++ routes/api.php
@@ -58,6 +58,5 @@ Route::prefix('v1')->group(function () {
     });
     Route::get('/logotipo', [EmpresaController::class, 'logotipo']);
     Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
     
 });
