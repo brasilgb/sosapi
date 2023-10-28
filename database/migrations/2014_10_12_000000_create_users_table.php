a676fdf41615d69135c55a60730586c3860e15f6
Anderson Brasil <andersobBrasil72@gmail.com>
2023-10-28 10:37:50 -0300

    push

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->tinyInteger('function');
+            $table->tinyInteger('role');
             $table->tinyInteger('status');
             $table->rememberToken();
             $table->timestamp('created_at')->useCurrent();
